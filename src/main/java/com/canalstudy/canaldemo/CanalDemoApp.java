d0b5fdff7d3f3b17d2f4eabf08c52ab7094796a5
chenzixin <15014324839@163.com>
2023-03-08 11:03:44 +0800

    update config

---
+++ src/main/java/com/canalstudy/canaldemo/CanalDemoApp.java
@@ -0,0 +1,10 @@
+package com.canalstudy.canaldemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalDemoApp {
+    public static void main(String[] args) {
+
+    }
+}
